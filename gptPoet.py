94c27200774c6ca24793acfbff3a839d07668108
coralexbadea <coralexbadea99@gmail.com>
2023-09-17 22:38:33 +0300

    sadf

--- gptPoet.py
+++ gptPoet.py
@@ -55,7 +55,6 @@ def read_json(json_path):
 
 
 def main():
-    jsonData = read_json('./prompts.json')
     begin_prompt = "explain to me in summary what this code does:"
     message = get_added_code()
 
@@ -64,10 +63,18 @@ def main():
 
     code_summary = make_openai_post(apy_key, prompt)
 
-    begin_prompt = "write me a short poem that comprehends this code description without doing any reference to programming or  programming languages:"
-    prompt = begin_prompt + code_summary 
-    code_poet = make_openai_post(apy_key, prompt)
-    print("code poet done\n\n", code_poet)
+    begin_prompt1 = "Write me a list of methaphoric words separated by comma describing the below code description. Remember I want only a list of methaphoric words separated by comma and nothing else:\nCode description:\n"
+    begin_prompt2 = "write me a short poem that comprehends this code description without doing any reference to programming or  programming languages:"
+
+    prompt1 = begin_prompt1 + code_summary 
+    prompt2 = begin_prompt2 + code_summary 
+
+    code_poet1 = make_openai_post(apy_key, prompt1)
+    code_poet2 = make_openai_post(apy_key, prompt2)
+
+    print("code poet done\n\n", code_poet1)
+    print("code poet done\n\n", code_poet2)
+
 
     # create_image(code_poet)
     
